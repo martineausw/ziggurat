d206e68b4ffd39c1398167785a3cc07c59a1b23e
Seth Martineau <martineausw@gmail.com>
2025-08-18 00:35:44 -0400

    edit sign function

--- src/sign.zig
+++ src/sign.zig
@@ -11,15 +11,15 @@ const Prototype = @import("prototype/Prototype.zig");
 pub fn sign(prototype: Prototype) fn (actual: anytype) fn (comptime return_type: type) type {
     return struct {
         pub fn validate(actual: anytype) fn (comptime return_type: type) type {
-            if (comptime prototype.eval(actual)) |result| {
+            if (prototype.eval(actual)) |result| {
                 if (!result) if (prototype.onFail) |onFail|
-                    comptime onFail(prototype, actual);
+                    onFail(prototype, actual);
             } else |err| {
                 if (prototype.onError) |onError|
-                    comptime onError(err, prototype, actual);
+                    onError(err, prototype, actual);
             }
 
-            return struct {
+            comptime return struct {
                 pub fn returns(comptime return_type: type) type {
                     return return_type;
                 }
